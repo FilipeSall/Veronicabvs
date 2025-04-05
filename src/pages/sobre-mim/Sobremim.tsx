0dd01e4c99f8da02feddadc63d753f4d4386743d
Filipe temporario pois será Jessica <filipesallesdev@gmail.com>
2025-04-05 14:10:54 -0300

    criando a seção de ferramentas com animação e colocando o basico da seção de empresas

--- src/pages/sobre-mim/Sobremim.tsx
+++ src/pages/sobre-mim/Sobremim.tsx
@@ -1,12 +1,18 @@
 import styles from './sobremim.module.scss';
 import veronicaImg from '../../assets/veronica.webp';
+import { Ferramentas } from '../../services/FerramentasData';
+import Ferramenta from './components/ferramenta/Ferramenta';
+import { FerramentaInterface } from '../../interfaces/components';
+import empresasImg from '../../assets/Empreas.webp'
 
 function Sobremim() {
     return (
         <main className={styles.sobremimContainer}>
+              {/* imagem*/}
             <section className={styles.imgSection}>
                 <img src={veronicaImg} className={styles.img} alt='Foto da Verônica de perfil cortada pela metade. Coloração preto e branca.' />
             </section>
+              {/* Seção do texto*/}
             <section className={styles.contentSection}>
                 <h1 className={styles.title}>Prazer, Verôn<span>ica</span></h1>
                 <p className={styles.para}><span>Bacharel</span> em arquitetura, <span>sou apaixonada por criar para pessoas através do design</span>. Pós-graduada em Design de Produtos Digitais, me encontrei na área, criando experiências guiada pela visão estratégica do <span>Data-Driven Design</span> desde 2022.</p>
@@ -18,6 +24,24 @@ function Sobremim() {
                 <p className="para">
                     Vamos conversar?
                 </p>
+
+                {/* Seção de ferramentas*/}
+                <section className={styles.ferramentasWrapper}>
+                    <div className={styles.ferramentasInner}>
+                        {Ferramentas.map((ferramenta: FerramentaInterface, i: number) => (
+                            <Ferramenta key={`original-${i}`} {...ferramenta} />
+                        ))}
+                        {Ferramentas.map((ferramenta: FerramentaInterface, i: number) => (
+                            <Ferramenta key={`duplicate-${i}`} {...ferramenta} />
+                        ))}
+                    </div>
+                </section>
+                
+                  {/* Seção das empresas*/}
+                <section className={styles.empresasWrapper}>
+                    <img src={empresasImg} className={styles.empresasImg} alt='Empresas na qual já trabalhei' />
+                </section>
+
             </section>
         </main>
     )

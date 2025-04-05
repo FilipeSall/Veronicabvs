2f0b10126d6d79630b7be5cfcbb00131de7d1bd2
Filipe temporario pois será Jessica <filipesallesdev@gmail.com>
2025-04-05 12:23:50 -0300

    colocando o title wrapper no ar

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,19 @@
-import styles from './Home.module.scss'
+import styles from './Home.module.scss';
 
 function Home() {
   return (
     <section className={styles.homeContainer}>
-       
+      <div className={styles.titleSectionContainer}>
+        <div className={styles.titlesWrapper}>
+            <p>Primavérn<span className={styles.textDegrade}>ica</span></p>
+            <h1 className={styles.title}>Verônica</h1>
+            <p>Outôn<span className={styles.textDegrade}>ica</span></p>
+            <p>Invérn<span className={styles.textDegrade}>ica</span></p>
+        </div>
+        <h2 className={styles.subtitle}>Product Designer</h2>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Home
+export default Home;

215fc0d9ffb6347265f8fdaff00321b30dc4d5e9
Filipe Reges de Salles <filipesallesdev@gmail.com>
2025-04-06 17:03:36 -0300

    Painel da pagina de cases finalizado

--- src/components/CasePanel/CasePanel.tsx
+++ src/components/CasePanel/CasePanel.tsx
@@ -1,25 +1,40 @@
 import { CasePanelInterface } from '../../interfaces/components';
 import { CasesInterface } from '../../interfaces/case';
 import { CasesData } from '../../services/casesData';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import styles from './casepanel.module.scss';
 
 function CasePanel({ data, nome, projeto }: CasePanelInterface) {
+
+    const location = useLocation();
+    const currentPath = location.pathname;
+
+    const getCurrentCaseId = () => {
+        if (currentPath.startsWith('/case/')) {
+            const id = currentPath.split('/').pop();
+            return id ? Number(id) : null;
+        }
+        return null;
+    };
+    
+    const currentCaseId = getCurrentCaseId();
+
     return (
         <div className={styles.container}>
             <div className={styles.infoWrapper}>
-                <p>{nome}</p>
+                <p className={styles.nome}>{nome}</p>
                 <div className={styles.projectDataWrapper}>
-                    <h1>{projeto}</h1>
-                    <p>{data}</p>
+                    <h1 className={styles.projeto}>{projeto}</h1>
+                    <span className={styles.dot}></span>
+                    <p className={styles.data}>{data}</p>
                 </div>
             </div>
             <div className={styles.navWrapper}>
                 {CasesData && CasesData.map((caseItem: CasesInterface, i: number) => (
-                    <Link 
-                        key={i} 
-                        to={`/case/${caseItem.id}`} 
-                        className={styles.caseNav}
+                    <Link
+                        key={i}
+                        to={`/case/${caseItem.id}`}
+                        className={`${styles.caseNav} ${currentCaseId === caseItem.id ? styles.active : ''}`}
                     >
                         {caseItem.id}
                     </Link>

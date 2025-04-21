fdbae358746c0ed5c68e629017edfbcc9f9c297f
Filipe Reges de Salles <filipesallesdev@gmail.com>
2025-04-21 19:55:14 -0300

    ajsutando links internos

--- src/components/internalLink/InternalLink.tsx
+++ src/components/internalLink/InternalLink.tsx
@@ -1,10 +1,17 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { LinkInterface } from '../../interfaces/components';
 import styles from './internallink.module.scss';
 
 function InternalLink({ path, text }: LinkInterface) {
     return (
-        <Link to={path} className={styles.link}>{text}</Link>
+        <NavLink 
+            to={path} 
+            className={({ isActive }) => 
+                isActive ? `${styles.link} ${styles.active}` : styles.link
+            }
+        >
+            {text}
+        </NavLink>
     )
 }
 

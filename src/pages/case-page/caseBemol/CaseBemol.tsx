d0d2bec0552b89bfd951754ddbdcaf457b2624c4
Filipe Reges de Salles <filipesallesdev@gmail.com>
2025-05-02 18:34:48 -0300

    style: Adicionando novo design system na seção de cases. regras mais claras

--- src/pages/case-page/caseBemol/CaseBemol.tsx
+++ src/pages/case-page/caseBemol/CaseBemol.tsx
@@ -2,12 +2,14 @@ import '../escopecasepage.scss';
 import tools from './tools';
 import ToolsIcon from '../components/ToolsIcon/ToolsIcon';
 import arrowUp from '../../../assets/svgs/arrowUp.svg';
+import TwoColumnGrid from '../components/TwoColumnGrid/TwoColumnGrid';
 
 function CaseBemol() {
     return (
-        <section className='pageContainer'>
+        <section className='page-container'>
+            {/* CASE HEADER*/}
             <div className='case-header' id='header-bemol'>
-                <p className='header-description'>
+                <p className='header-description' style={{ borderLeft: '1px solid #404040' }}>
                     <span>Contexto:</span> Atuação abrangente em <span>conta digital</span> (Conta Bemol), com responsabilidade direta sobre o produto de empréstimos (Empréstimo Pessoal) — conduzindo <span>todo o ciclo de design</span>, desde pesquisas na etapa de discoberta à entrega e estratégia do acompanhamento de métricas pós lançamento.
                 </p>
 
@@ -21,9 +23,106 @@ function CaseBemol() {
                 </div>
             </div>
             {/*SUMMARY */}
-        
+
+            {/*1.1 Resgatando clientes no fluxo de empréstimo por notificações segmentadas*/}
+            <section className='chapter' style={{ backgroundColor: '#4c31e0', color: '#FFFFFF' }}>
+                <div className='case-title__wrapper'>
+                    <h2 className='case-title'>1.1 Resgatando clientes no fluxo de empréstimo por notificações segmentadas</h2>
+                    <p className='case-subtitle'><span>Apoio:</span> Equipe de Marketing e equipe de Dados.</p>
+                </div>
+                <div className='subsection'>
+                    <h3 className="bullet-white">Resultado</h3>
+                    <div className='subsection-content'>
+                        VIDEO CONTAINER
+                    </div>
+                </div>
+            </section>
+            {/*1.2 Outras entregas*/}
+            <section className='chapter' style={{ backgroundColor: '#FFFFFF', color: '#000000' }}>
+                <div className='case-title__wrapper'>
+                    <h2 className='case-title'>1.2 Outras entregas</h2>
+                </div>
+
+                <div className='subsection'>
+                    <h3 className="bullet-black">Fluxogramas completos do produto Empréstimo Pessoal</h3>
+                    <div className='subsection-content'>
+                        VIDEO CONTAINER
+                    </div>
+                </div>
+
+                <div className='subsection'>
+                    <h3 className="bullet-black">Diagnóstico e oportunidade</h3>
+                    <div className='subsection-content'>
+                        TEXT CONTAINER
+                    </div>
+                </div>
+
+                <div className='subsection'>
+                    <h3 className="bullet-black">Entrega de valor</h3>
+                    <div className='subsection-content'>
+                        TEXT CONTAINER
+                    </div>
+                </div>
+
+            </section>
+            {/*1.3 Outras entregas*/}
+            <section className='chapter' style={{ backgroundColor: '#4c31e0', color: '#FFFFFF' }}>
+
+                <div className='case-title__wrapper'>
+                    <h2 className='case-title'>1.3 Outras entregas</h2>
+                </div>
+
+                <div className="subsection noGap">
+                    <p className='subsection-description'>
+                        Propostas guiadas pela abordagem de <span>Data-Driven Design</span>
+                        — desde a identificação da dor, validação do problema, até a definição
+                        da solução e plano de acompanhamento pós lançamento.
+                    </p>
+                    <TwoColumnGrid
+                        leftContent={
+                            <>
+                                <div className="metric-content">
+                                    <h4>Cálculo e análise do NPS da Conta Digital do ano de 2023.</h4>
+                                    IMAGEM COMPONENT
+                                </div>
+                            </>
+                        }
+                        rightContent={
+                            <div className="metric-content">
+                                <h4>Apresentações mensais da análise da taxa de cancelamento de contas.</h4>
+                                IMAGEM COMPONENT
+                            </div>
+                        }
+                    />
+                </div>
+
+            </section>
+            {/*1.4 Escolha da data do vencimento das parcelas de empréstimo (prévia) */}
+
+            <section className='chapter' style={{ backgroundColor: '#FFFFFF', color: '#000000' }}>
+                <div className='case-title__wrapper'>
+                    <h2 className='case-title'>1.4 Escolha da data do vencimento das parcelas de empréstimo (prévia)</h2>
+                    <p className='case-subtitle'><span>Exclusivo</span> para apresentação pessoal, entre em contato comigo e agende</p>
+                    <ul className='list-wrapper' style={{ color: '#7F7F7F' }}>
+                        <li>Identificação da dor</li>
+                        <li>Meu processo de design</li>
+                        <li>Pesquisa e Benchmarking</li>
+                        <li>Coleta e análise de dados</li>
+                        <li>Design System <span>(documentação técnica de novo componente)</span></li>
+                        <li>Teste com usuários</li>
+                        <li>Aprendizados</li>
+                    </ul>
+
+                    <div className='subsection'>
+                        <div className='subsection-content'>
+                            VIDEO CONTAINER
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <p className='footer-text'>💡Entre em contato e agente uma apresentação,por algum dos botões abaixo!</p>
             <a href='#header-bemol' className='scrollup-case'>
-                <img src={arrowUp} alt='Seta para cima'/>
+                <img src={arrowUp} alt='Seta para cima' />
             </a>
         </section>
     )

68c1ae840d19d5646f91f16b0e99c8d59efce507
Filipe Reges de Salles <filipesallesdev@gmail.com>
2025-05-07 20:05:16 -0300

    alterando o Header, Footer e Sobre-mim

--- src/pages/sobre-mim/Sobremim.tsx
+++ src/pages/sobre-mim/Sobremim.tsx
@@ -3,7 +3,9 @@ import veronicaImg from '../../assets/veronica.webp';
 import { Ferramentas } from '../../services/FerramentasData';
 import Ferramenta from './components/ferramenta/Ferramenta';
 import { FerramentaInterface } from '../../interfaces/components';
-import empresasImg from '../../assets/Empreas.webp'
+import empresasImg from '../../assets/EMPRESAS.png'
+import newWindowIcon from '../../assets/svgs/new-window-black.svg';
+import pdfCurriculo from '../../assets/pdfs/curriculo.pdf';
 
 function Sobremim() {
     return (
@@ -15,19 +17,31 @@ function Sobremim() {
             {/* Seção do texto*/}
             <section className={styles.contentSection}>
                 <h1 className={styles.title}>Prazer, Verôn<span>ica</span></h1>
-                <p className={styles.para}><span>Bacharel</span> em arquitetura, <span>sou apaixonada por criar para pessoas através do design</span>. <span>Pós-graduada</span> em Design de Produtos Digitais, me encontrei na área, criando experiências guiada pela visão estratégica do <span>Data-Driven Design</span> desde 2022.</p>
+                <p className={styles.para}>Bacharel em Arquitetura, sou <span>apaixonada por criar para pessoas através do Design</span>.</p>
+                <p className={styles.para}><span>Pós-graduada em Design de Produtos Digitais</span>, atuo desde 2022 desenvolvendo experiências orientadas por dados com base no <span>Data-Driven Design</span>. Aprofundo esse olhar como <span>graduanda em Ciência de Dados</span>, unindo sensibilidade estética e pensamento analítico para criar soluções centradas no usuário</p>
 
                 <p className={styles.para}>
                     Minha trajetória inclui atuação em <span>e-commerce</span>, o marketing digital, <span>Fintech</span> e negócios, em empresas B2C e B2B, com vivências nacionais e internacionais.
                 </p>
 
                 <p className={styles.para}>
-                    Vamos conversar?
+                    <span>Vamos conversar?</span>
                 </p>
+
+                <div className={styles.textWrapperContainer}>
+                    <div className="bullet-point">•</div>
+                    <p>Acesse meu currículo </p>
+                    <a href={pdfCurriculo} target='_blank'>
+                        <img alt='Nova janela icone' className={styles.newWindowBtn} src={newWindowIcon} />
+                    </a>
+                </div>
 
                 {/* Seção de ferramentas*/}
                 <section className={styles.ferramentasSection}>
-                    <p>Principais ferramentas:</p>
+                    <div className={styles.textWrapperContainer}>
+                        <div className="bullet-point">•</div>
+                        <p>Principais ferramentas:</p>
+                    </div>
                     <div className={styles.ferramentasWrapper}>
                         <div className={styles.ferramentasInner}>
                             {Ferramentas.map((ferramenta: FerramentaInterface, i: number) => (
@@ -41,9 +55,17 @@ function Sobremim() {
                 </section>
 
                 {/* Seção das empresas*/}
-                <section className={styles.empresasWrapper}>
-                    <p>Empresas por onde passei:</p>
-                    <img src={empresasImg} className={styles.empresasImg} alt='Empresas na qual já trabalhei' />
+                <section className={styles.empresasSection}>
+                    <div className={styles.textWrapperContainer}>
+                        <div className={styles.bulletPoint}>•</div>
+                        <p>Empresas por onde passei:</p>
+                    </div>
+                    <div className={styles.empresasWrapper}>
+                        <div className={styles.empresasInner}>
+                            <img src={empresasImg} className={styles.empresasImg} alt='Empresas na qual já trabalhei' />
+                            <img src={empresasImg} className={styles.empresasImg} alt='Empresas na qual já trabalhei' />
+                        </div>
+                    </div>
                 </section>
             </section>
         </main>

1c944c0dadc13194ad7ae5b0e16bc452b3733d78
Filipe Salles <filipesallesdev@gmail.com>
2025-07-06 14:35:13 -0300

    ajustando a pagina sobremim versao: 01-07

--- src/pages/sobre-mim/Sobremim.tsx
+++ src/pages/sobre-mim/Sobremim.tsx
@@ -1,5 +1,5 @@
 import styles from './sobremim.module.scss';
-import veronicaImg from '../../assets/veronica.webp';
+import veronicaImg from '../../assets/veronica.png';
 import { Ferramentas } from '../../services/FerramentasData';
 import Ferramenta from './components/ferramenta/Ferramenta';
 import { FerramentaInterface } from '../../interfaces/components';
@@ -17,27 +17,26 @@ function Sobremim() {
             {/* Seção do texto*/}
             <section className={styles.contentSection}>
                 <div className={styles.textWrapper}>
-                    <h1 className={styles.title}>Prazer, Verôn<span>ica</span></h1>
+                    <h1 className={styles.title}>Prazer, Verô<span>nica</span></h1>
                     <p className={styles.para}>Bacharel em Arquitetura, sou <span>apaixonada por criar para pessoas através do Design</span>.</p>
-                    <p className={styles.para}><span>Pós-graduada em Design de Produtos Digitais</span>, atuo desde 2022 desenvolvendo experiências orientadas por dados com base no <span>Data-Driven Design</span>. Aprofundo esse olhar como <span>graduanda em Ciência de Dados</span>, unindo sensibilidade estética e pensamento analítico para criar soluções centradas no usuário</p>
+                    <p className={styles.para}><span>Pós-graduada em Design de Produtos Digitais</span>, atuo desde 2022 desenvolvendo experiências orientadas por dados com base no <span>Data-Driven Design</span>. Aprofundo esse olhar como <span>graduanda em Ciência de Dados</span>, unindo sensibilidade estética e pensamento analítico para criar soluções centradas no usuário.</p>
                     <p className={styles.para}>
                         Minha trajetória inclui atuação em <span>e-commerce</span>, o marketing digital, <span>Fintech</span> e negócios, em empresas B2C e B2B, com vivências nacionais e internacionais.
                     </p>
-                    <p className={styles.para}>
-                        <span>Vamos conversar?</span>
-                    </p>
-                </div>
-
-                <div className={styles.textWrapperContainer}>
-                    <div className="bullet-point">•</div>
-                    <a
-                        href={pdfCurriculo}
-                        target='_blank'
-                        className={styles.curriculoLink}
-                    >
-                        <p>Acesse meu currículo </p>
-                        <img alt='Nova janela icone' className={styles.newWindowBtn} src={newWindowIcon} />
-                    </a>
+                    <div className={styles.paraConversar}>
+                        <p><span>Vamos conversar?</span></p>
+                        <div className={styles.textWrapperContainer}>
+                            <div className={styles.bulletPoint}>•</div>
+                            <a
+                                href={pdfCurriculo}
+                                target='_blank'
+                                className={styles.curriculoLink}
+                            >
+                                <span>Acesse meu currículo </span>
+                                <img alt='Nova janela icone' className={styles.newWindowBtn} src={newWindowIcon} />
+                            </a>
+                        </div>
+                    </div>
                 </div>
 
                 {/* Seção das empresas*/}
@@ -57,7 +56,7 @@ function Sobremim() {
                 {/* Seção de ferramentas*/}
                 <section className={styles.ferramentasSection}>
                     <div className={styles.textWrapperContainer}>
-                        <div className="bullet-point">•</div>
+                        <div className={styles.bulletPoint}>•</div>
                         <p>Principais ferramentas:</p>
                     </div>
                     <div className={styles.ferramentasWrapper}>

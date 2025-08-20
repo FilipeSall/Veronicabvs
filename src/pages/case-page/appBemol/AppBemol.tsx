f58846ac1247b45ff79bdba0258a262b946d7d11
Filipe Salles <filipe.salles@seatecnologia.com.br>
2025-08-20 20:57:20 -0300

    migrando estrutura caseBemol para a AppBemol

--- src/pages/case-page/appBemol/AppBemol.tsx
+++ src/pages/case-page/appBemol/AppBemol.tsx
@@ -1,10 +1,8 @@
-import styles from './appbemol.module.scss';
 import { summary } from './summary';
 import '../escopecasepage.scss';
 import tools from './tools';
 import PageHeader from '../../components/page-header/PageHeader';
 import Summary from '../../components/summary/Summary';
-import CaseTitle from '../../components/case-title/CaseTitle';
 import VideoLegend from '../../components/video-legend/VideoLegend';
 import videoAutocoplete from '../../../assets/appBemol/autocomplete.mp4';
 import redArrow from '../../../assets/svgs/redArrow.svg';
@@ -16,6 +14,11 @@ import CardsEnumerados from '../components/cardsEnumerados/CardsEnumerados';
 import { useScrollToElement } from '../../../hooks/useScrollToElement';
 import ScrollToTop from '../../components/scroll-to-top/ScrollToTop';
 import Carrossel from '../components/carrossel/Carrossel';
+import Chapter from '../components/Chapter/Chapter';
+import ContentBlock from '../components/ContentBlock/ContentBlock';
+import CaseTitleHeader from '../components/CaseTitleHeader/CaseTitleHeader';
+import ContentSection from '../components/contentSection/ContentSection ';
+import ContentWrapper from '../components/ContentWrapper/ContentWrapper';
 
 function AppBemol() {
 
@@ -33,160 +36,167 @@ function AppBemol() {
             <Summary items={summary.items} bg={summary.bg} color={summary.color} totalReading={totalReading} />
 
             {/*2.1 Autocomplete para reconhecimento de clientes com Conta Bemol*/}
-            <section className='chapter' id='chapter-2-1'>
-                <div className='chapter-content'>
-                    <CaseTitle
-                        title={summary.items[0].title}
-                        subTitle={summary.items[0].subtitle}
-                        id={summary.items[0].id}
-                    />
+            <Chapter id='chapter-2-1'>
+                <CaseTitleHeader
+                    id={summary.items[0].id}
+                    title={summary.items[0].title}
+                    subtitle={summary.items[0].subtitle}
+                />
 
+                <ContentBlock>
                     <VideoLegend
                         src={videoAutocoplete}
                         legend='Solução: Reconhecimento de cliente com Conta Bemol durante o processo de contratação de empréstimo por preenchimento inteligente.'
                     />
-                </div>
+                </ContentBlock>
 
-                <div className={styles.appBemolDiv}>
-                    <h3 className={styles.appTitle}>CONTEXTO</h3>
-                    <p className={styles.appText}>A Bemol é uma empresa amazonense com forte atuação nos setores de varejo <span>(BSA)</span> e serviços financeiros <span>(BSF)</span>, oferecendo uma experiência integrada por meio de dois aplicativos principais:
-                        <br />
-                        <br />
-                        <span>App Bemol:</span> Aplicativo voltado para compras online (e-commerce). <br /> <span>Conta Bemol:</span> Destinado à gestão de serviços financeiros (conta digital)
-                        <br />
-                        <br />
-                        O produto de <span>Empréstimo Pessoal</span> está disponível em ambos os apps, permitindo que os usuários contratem crédito de forma prática e digital
-                    </p>
-                </div>
+                <ContentBlock>
+                    <ContentSection
+                        title="CONTEXTO"
+                        paragraphs={[
+                            "A Bemol é uma empresa amazonense com forte atuação nos setores de varejo **(BSA)** e serviços financeiros **(BSF)**, oferecendo uma experiência integrada por meio de dois aplicativos principais:",
+                            "**App Bemol:** Aplicativo voltado para compras online (e-commerce).",
+                            "**Conta Bemol:** Destinado à gestão de serviços financeiros (conta digital)",
+                            "O produto de **Empréstimo Pessoal** está disponível em ambos os apps, permitindo que os usuários contratem crédito de forma prática e digital"
+                        ]}
+                    />
+                </ContentBlock>
 
-                <div className={styles.appBemolDiv}>
-                    <h3 className={styles.appTitle}>DESAFIO E OPORTUNIDADE</h3>
-                    <p className={styles.appText}>Uma das <span>metas estratégicas</span> da empresa para o ano de 2024 era o aumento da base de contas Bemol ativas.
-                        <br />
-                        <br />
-                        Em <span>parceria</span> com a equipe de dados, constatamos uma <span>taxa muito baixa (1,8%)</span> de escolha da Conta Bemol como destino do crédito, mesmo entre usuários que já possuíam conta ativa no ecossistema (no último semestre)
-                        <br />
-                        <br />
-                        A situação revelou uma <span>oportunidade</span> de atuação com foco em <span>incentivar o cliente</span> com conta ativa a escolher a Conta Bemol como destino do crédito, em vez de contas de terceiros.
-                    </p>
-                </div>
+                <ContentBlock>
+                    <ContentSection
+                        title="DESAFIO E OPORTUNIDADE"
+                        paragraphs={[
+                            "Uma das **metas estratégicas** da empresa para o ano de 2024 era o aumento da base de contas Bemol ativas.",
+                            "Em **parceria** com a equipe de dados, constatamos uma **taxa muito baixa (1,8%)** de escolha da Conta Bemol como destino do crédito, mesmo entre usuários que já possuíam conta ativa no ecossistema (no último semestre)",
+                            "A situação revelou uma **oportunidade** de atuação com foco em **incentivar o cliente** com conta ativa a escolher a Conta Bemol como destino do crédito, em vez de contas de terceiros."
+                        ]}
+                    />
+                </ContentBlock>
 
-                <div className={styles.appBemolDiv}>
-                    <h3 className={styles.appTitle}>PROCESSO</h3>
-                    <p className={styles.appText}>A solução foi desenvolvida seguindo o modelo <span>Double Diamond adaptado ao Data-Driven Design</span>, que inclui uma fase inicial de planejamento estratégico e uma final de acompanhamento contínuo.
-                    </p>
-                </div>
-
-                <div className={styles.appBemolDiv}>
+                <ContentBlock>
+                    <ContentSection
+                        title="PROCESSO"
+                        paragraphs={[
+                            "A solução foi desenvolvida seguindo o modelo **Double Diamond adaptado ao Data-Driven Design**, que inclui uma fase inicial de planejamento estratégico e uma final de acompanhamento contínuo."
+                        ]}
+                    />
                     <ProcessFlowContainer
                         arrowColored={redArrow}
                         color="#E94545"
                         lists={processData}
                     />
-                </div>
+                </ContentBlock>
 
-                <div className={styles.appBemolDiv}>
-                    <h3 className={styles.appTitle}>DESIGN SYSTEM</h3>
-                    <p className={styles.appText}>Contribuí para a evolução do Design System com a <span>documentação técnica</span> e adaptação do componente Datepicker, originalmente do <span>Material 3 (Google)</span>, para a identidade da marca.
-                    </p>
-                </div>
+                <ContentBlock>
+                    <ContentSection
+                        title="DESIGN SYSTEM"
+                        paragraphs={[
+                            "Contribuí para a evolução do Design System com a **documentação técnica** e adaptação do componente Datepicker, originalmente do **Material 3 (Google)**, para a identidade da marca."
+                        ]}
+                    />
+                    <div className="subsection-img__wrapper">
+                        <img src={desingSystemImg} alt="design sistem" style={{ width: '100%' }} />
+                    </div>
+                </ContentBlock>
 
-                <div className={styles.appBemolDiv}>
-                    <img src={desingSystemImg} alt="design sistem" className={styles.appBemolImg} />
-                </div>
-
-                <div className={styles.appBemolDiv}>
-                    <h3 className={styles.appTitle}>NEGÓCIO X EXPERIÊNCIA</h3>
-                    <p className={styles.appText}>Elaborei duas abordagens para o componente: uma com foco em conversão direta, favorecendo objetivos de <span>negócio (Business First)</span>, e outra com uma comunicação mais fluida e informativa, priorizando a <span>experiência do usuário (Experience First)</span>.
-                        <br />
-                        <br />
-                        Apresentei ambas aos stakeholders e, após alinhamento, seguimos com a opção mais alinhada aos objetivos de negócio (Opção 1), embora a outra solução oferecesse uma experiência mais completa para o usuário.
-                    </p>
-                </div>
-
-                <div className={styles.appBemolDiv}>
+                <ContentBlock>
+                    <ContentSection
+                        title="NEGÓCIO X EXPERIÊNCIA"
+                        paragraphs={[
+                            "Elaborei duas abordagens para o componente: uma com foco em conversão direta, favorecendo objetivos de **negócio (Business First)**, e outra com uma comunicação mais fluida e informativa, priorizando a **experiência do usuário (Experience First)**.",
+                            "Apresentei ambas aos stakeholders e, após alinhamento, seguimos com a opção mais alinhada aos objetivos de negócio (Opção 1), embora a outra solução oferecesse uma experiência mais completa para o usuário."
+                        ]}
+                    />
                     <NegocioExperiencia />
-                </div>
+                </ContentBlock>
 
-                <div className={styles.appBemolDiv}>
-                    <h3 className={styles.appTitle}>PLANO DE ACOMPANHAMENTO</h3>
-                    <p className={styles.appText}>Para acompanhar a evolução e impacto da solução no negócio, elaborei um plano de monitoramento mensal, analisando KPIs:
-                    </p>
-                    <ul className={styles.arrowList}>
-                        <li><span>→</span>Taxa de adesão à Conta Bemol;</li>
-                        <li><span>→</span>Conversão no fluxo de contratação;</li>
-                        <li><span>→</span>Fricção no processo de reconhecimento;</li>
-                        <li><span>→</span>Feedback qualitativo dos usuários (via pesquisas contínuas).</li>
-                    </ul>
-                </div>
+                <ContentBlock>
+                    <ContentWrapper title="PLANO DE ACOMPANHAMENTO">
+                        <p className="content-text">
+                            Para acompanhar a evolução e impacto da solução no negócio, elaborei um plano de monitoramento mensal, analisando KPIs:
+                        </p>
+                        <ul className="content-list">
+                            <li><span>Taxa de adesão</span> à Conta Bemol</li>
+                            <li><span>Conversão</span> no fluxo de contratação</li>
+                            <li><span>Fricção</span> no processo de reconhecimento</li>
+                            <li><span>Feedback qualitativo</span> dos usuários (via pesquisas contínuas)</li>
+                        </ul>
+                    </ContentWrapper>
+                </ContentBlock>
 
-                <div className={styles.appBemolDiv}>
-                    <h3 className={styles.appTitle}>MÉTRICAS E MELHORIAS</h3>
-                    <p className={styles.appText}>Após o lançamento, acompanhamos os KPI's para medir o sucesso da solução implementada em relação aos objetivos do negócio e da experiência do usuário. Destaque para:
-                        <br />
-                        <br />
-                        →  A escolha da Conta Bemol como destino do crédito contratado <span>aumentou de 1,8% para 14,6%</span> nas primeiras semanas após a implementação.
-                    </p>
-                </div>
-            </section>
+                <ContentBlock>
+                    <ContentWrapper title="MÉTRICAS E MELHORIAS">
+                        <p className="content-text">
+                            Após o lançamento, acompanhamos os KPI's para medir o sucesso da solução implementada em relação aos objetivos do negócio e da experiência do usuário. Destaque para:
+                        </p>
+                        <p className="content-text">
+                            A escolha da Conta Bemol como destino do crédito contratado <span>aumentou de 1,8% para 14,6%</span> nas primeiras semanas após a implementação.
+                        </p>
+                    </ContentWrapper>
+                </ContentBlock>
+            </Chapter>
 
             {/*2.2 Redesign das telas de contratação de empréstimos via aplicativo e-commerce*/}
-            <section className='chapter' id='chapter-2-2'>
-                <div className='chapter-content'>
-                    <CaseTitle
-                        title={summary.items[1].title}
-                        subTitle={summary.items[1].subtitle}
-                        id={summary.items[1].id}
-                    />
-                </div>
+            <Chapter id='chapter-2-2'>
+                <CaseTitleHeader
+                    id={summary.items[1].id}
+                    title={summary.items[1].title}
+                    subtitle={summary.items[1].subtitle}
+                />
 
-                <div className={styles.appBemolDiv}>
-                    <h3 className={styles.appTitle}>DIAGNÓSTICO</h3>
-                    <p className={styles.appText}>Identificação de pontos de dor nas telas originais, como:
-                    </p>
-                    <ul className={styles.arrowList}>
-                        <li><span>→</span>Experiência fragmentada e pouco intuitiva;</li>
-                        <li><span>→</span>Falta de clareza na exibição das informações (impactando a confiança do usuário);</li>
-                        <li><span>→</span>Etapas desnecessárias ou confusas(que levavam ao abandono do fluxo);</li>
-                        <li><span>→</span>Barreiras na finalização da contratação (potencialmente motivando a queda na taxa de conversão).</li>
-                    </ul>
-                </div>
-                <div className={styles.appBemolDiv}>
-                    <h3 className={styles.appTitle}>ANTES X DEPOIS</h3>
-                    <Carrossel
-                        slides={carrosselData}
-                        title="Tela inicial do fluxo de contratação"
-                        dotColor='#F23434'
-                    />
-                </div>
-                <div className={styles.appBemolDiv}>
-                    <h3 className={styles.appTitle}>DESAFIO E OBJETIVO</h3>
-                    <p className={styles.appText}>O redesenho envolveu a responsabilidade de <span>otimizar o fluxo</span> do produto mais rentável da empresa.
-                        <br />
-                        <br />
-                        O objetivo era <span>equilibrar eficiência do negócio e experiência do usuário</span>, garantindo que as mudanças impulsionassem tanto a conversão quanto a satisfação
-                    </p>
-                </div>
+                <ContentBlock>
+                    <ContentWrapper title="DIAGNÓSTICO">
+                        <p className="content-text">
+                            Identificação de pontos de dor nas telas originais, como:
+                        </p>
+                        <ul className="content-list">
+                            <li><span>Experiência fragmentada</span> e pouco intuitiva</li>
+                            <li><span>Falta de clareza</span> na exibição das informações (impactando a confiança do usuário)</li>
+                            <li><span>Etapas desnecessárias</span> ou confusas (que levavam ao abandono do fluxo)</li>
+                            <li><span>Barreiras na finalização</span> da contratação (potencialmente motivando a queda na taxa de conversão)</li>
+                        </ul>
+                    </ContentWrapper>
+                </ContentBlock>
 
-                <div className={styles.appBemolDiv}>
+                <ContentBlock>
+                    <ContentWrapper title="ANTES X DEPOIS">
+                        <Carrossel
+                            slides={carrosselData}
+                            title="Tela inicial do fluxo de contratação"
+                            dotColor='#F23434'
+                        />
+                    </ContentWrapper>
+                </ContentBlock>
+
+                <ContentBlock>
+                    <ContentSection
+                        title="DESAFIO E OBJETIVO"
+                        paragraphs={[
+                            "O redesenho envolveu a responsabilidade de **otimizar o fluxo** do produto mais rentável da empresa.",
+                            "O objetivo era **equilibrar eficiência do negócio e experiência do usuário**, garantindo que as mudanças impulsionassem tanto a conversão quanto a satisfação"
+                        ]}
+                    />
+                </ContentBlock>
+
+                <ContentBlock>
                     <CardsEnumerados
                         cards={cardsData}
                         title='MELHORIAS'
                         color='#F23434'
                     />
-                </div>
+                </ContentBlock>
 
-                <div className={styles.appBemolDiv}>
-                    <h3 className={styles.appTitle}>PRÓXIMOS PASSOS</h3>
-                    <p className={styles.appText}>→ Acompanhamento de métricas pós-lançamento
-                        Como taxa de conversão, tempo médio no fluxo e taxa de abandono;
-                        <br />
-                        <br />
-                        → Testes de usabilidade com usuários reais
-                        Para validar as melhorias e identificar novas oportunidades.
-                    </p>
-                </div>
-            </section>
+                <ContentBlock>
+                    <ContentWrapper title="PRÓXIMOS PASSOS">
+                        <p className="content-text">
+                            <span>Acompanhamento de métricas pós-lançamento:</span> Como taxa de conversão, tempo médio no fluxo e taxa de abandono.
+                        </p>
+                        <p className="content-text">
+                            <span>Testes de usabilidade com usuários reais:</span> Para validar as melhorias e identificar novas oportunidades.
+                        </p>
+                    </ContentWrapper>
+                </ContentBlock>
+            </Chapter>
 
             {/*FOOTER*/}
             <p className='footer-text'>

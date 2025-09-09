0ad287a30d2975ae3d9ca10ad07f8f072c5813fe
Filipe Salles <filipe.salles@seatecnologia.com.br>
2025-09-08 21:02:38 -0300

    feat: step 3.5

--- src/pages/case-page/components/media/StepsImgFlow/StepsImgFlow.tsx
+++ src/pages/case-page/components/media/StepsImgFlow/StepsImgFlow.tsx
@@ -5,6 +5,7 @@ import phone3 from "../../../../../assets/melliuz/phone3.webp";
 import phone4 from "../../../../../assets/melliuz/phone4.webp";
 import phone5 from "../../../../../assets/melliuz/phone5.webp";
 import phone6 from "../../../../../assets/melliuz/phone6.webp";
+import phone7 from "../../../../../assets/melliuz/phone7.webp";
 import { ArcherContainer, ArcherElement } from "react-archer";
 
 function StepsImgFlow() {
@@ -15,106 +16,136 @@ function StepsImgFlow() {
   };
 
   return (
-    <ArcherContainer strokeColor="#BBBBBB" strokeWidth={1} strokeDasharray="8">
-      <div className={styles.container}>
-        <div className={styles.stepWrapper}>
-          <img src={phone1} alt="tela de extrato" />
-          <ArcherElement
-            id="step1"
-            relations={[
-              {
-                targetId: "step2",
-                sourceAnchor: "middle",
-                targetAnchor: "middle",
-                style: archerLineStyle,
-              },
-            ]}
-          >
-            <div className={styles.number}>1.</div>
-          </ArcherElement>
-          <p>Na tela de Extrato, acessa ícone de doação na parte superior</p>
+    <ArcherContainer
+      strokeColor="#BBBBBB"
+      strokeWidth={1}
+      strokeDasharray="8"
+      svgContainerStyle={{
+        pointerEvents: "none",
+      }}
+    >
+      <div className={styles.mainWrapper}>
+        <div className={styles.container}>
+          <div className={styles.stepWrapper}>
+            <img src={phone1} alt="tela de extrato" />
+            <ArcherElement
+              id="step1"
+              relations={[
+                {
+                  targetId: "step2",
+                  sourceAnchor: "right",
+                  targetAnchor: "left",
+                  style: archerLineStyle,
+                },
+              ]}
+            >
+              <div className={styles.number}>1.</div>
+            </ArcherElement>
+            <p>Na tela de Extrato, acessa ícone de doação na parte superior</p>
+          </div>
+          <div className={styles.stepWrapper}>
+            <img src={phone2} alt="tela de extrato" />
+            <ArcherElement
+              id="step2"
+              relations={[
+                {
+                  targetId: "step3",
+                  sourceAnchor: "right",
+                  targetAnchor: "left",
+                  style: archerLineStyle,
+                },
+              ]}
+            >
+              <div className={styles.number}>2.</div>
+            </ArcherElement>
+            <p>Opta pelo botão sugestivo para doar 100 reais</p>
+          </div>
+          <div className={styles.stepWrapper}>
+            <img src={phone3} alt="Escolha uma parceria" />
+            <ArcherElement
+              id="step3"
+              relations={[
+                {
+                  targetId: "step4",
+                  sourceAnchor: "right",
+                  targetAnchor: "left",
+                  style: archerLineStyle,
+                },
+              ]}
+            >
+              <div className={styles.number}>3.</div>
+            </ArcherElement>
+            <p>
+              Escolhe uma Ong parceira, de sua preferência, para receber a
+              doação
+            </p>
+          </div>
+          <div className={styles.stepWrapper}>
+            <img src={phone4} alt="Passo 4" />
+            <ArcherElement
+              id="step4"
+              relations={[
+                {
+                  targetId: "step5",
+                  sourceAnchor: "right",
+                  targetAnchor: "left",
+                  style: archerLineStyle,
+                },
+              ]}
+            >
+              <div className={styles.number}>4.</div>
+            </ArcherElement>
+            <p>Opta por não enviar uma mensagem de texto personalizada</p>
+          </div>
+          <div className={styles.stepWrapper}>
+            <img src={phone5} alt="Passo 5" />
+            <ArcherElement
+              id="step5"
+              relations={[
+                {
+                  targetId: "step6",
+                  sourceAnchor: "right",
+                  targetAnchor: "left",
+                  style: archerLineStyle,
+                },
+              ]}
+            >
+              <div className={styles.number}>5.</div>
+            </ArcherElement>
+            <p>Confirma doação com sua senha</p>
+          </div>
+          <div className={styles.stepWrapper}>
+            <img src={phone6} alt="Passo 6" />
+            <ArcherElement
+              id="step6"
+              relations={[
+                {
+                  targetId: "step7",
+                  sourceAnchor: "bottom",
+                  targetAnchor: "left",
+                  style: archerLineStyle,
+                },
+              ]}
+            >
+              <div className={styles.number}>6.</div>
+            </ArcherElement>
+            <p>Opta por compartilhar sua doação</p>
+          </div>
         </div>
 
-        <div className={styles.stepWrapper}>
-          <img src={phone2} alt="tela de extrato" />
-          <ArcherElement
-            id="step2"
-            relations={[
-              {
-                targetId: "step3",
-                sourceAnchor: "middle",
-                targetAnchor: "middle",
-                style: archerLineStyle,
-              },
-            ]}
-          >
-            <div className={styles.number}>2.</div>
-          </ArcherElement>
-          <p>Opta pelo botão sugestivo para doar 100 reais</p>
-        </div>
-
-        <div className={styles.stepWrapper}>
-          <img src={phone3} alt="Escolha uma parceria" />
-          <ArcherElement
-            id="step3"
-            relations={[
-              {
-                targetId: "step4",
-                sourceAnchor: "middle",
-                targetAnchor: "middle",
-                style: archerLineStyle,
-              },
-            ]}
-          >
-            <div className={styles.number}>3.</div>
-          </ArcherElement>
-          <p>
-            Escolhe uma Ong parceira, de sua preferência, para receber a doação
-          </p>
-        </div>
-
-        <div className={styles.stepWrapper}>
-          <img src={phone4} alt="Passo 4" />
-          <ArcherElement
-            id="step4"
-            relations={[
-              {
-                targetId: "step5",
-                sourceAnchor: "middle",
-                targetAnchor: "middle",
-                style: archerLineStyle,
-              },
-            ]}
-          >
-            <div className={styles.number}>4.</div>
-          </ArcherElement>
-          <p>Opta por não enviar uma mensagem de texto personalizada</p>
-        </div>
-
-        <div className={styles.stepWrapper}>
-          <img src={phone5} alt="Passo 5" />
-          <ArcherElement
-            id="step5"
-            relations={[
-              {
-                targetId: "step6",
-                sourceAnchor: "middle",
-                targetAnchor: "middle",
-                style: archerLineStyle,
-              },
-            ]}
-          >
-            <div className={styles.number}>5.</div>
-          </ArcherElement>
-          <p>Confirma doação com sua senha</p>
-        </div>
-
-        <div className={styles.stepWrapper}>
-          <img src={phone6} alt="Passo 6" />
-          <ArcherElement id="step6">
-            <div className={styles.number}>6.</div>
-          </ArcherElement>
-          <p>Opta por compartilhar sua doação</p>
+        <div className={styles.secondContainer}>
+          <div className={styles.mainStepContainer}>
+            <img src={phone7} alt="Passo 7" />
+            <div className={styles.textWrapper}>
+              <ArcherElement id="step7">
+                <div className={styles.number}>7.</div>
+              </ArcherElement>
+              <p>
+                Compartilha arte pronta em sua rede social, de forma prática e
+                rápida
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </ArcherContainer>

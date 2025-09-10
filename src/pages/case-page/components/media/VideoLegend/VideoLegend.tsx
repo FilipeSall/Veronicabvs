6735ddf361eb0dd6a4146eb8717e690fd47c74f3
Filipe Salles <filipe.salles@seatecnologia.com.br>
2025-09-10 11:23:56 -0300

    remoe skelleton video

--- src/pages/case-page/components/media/VideoLegend/VideoLegend.tsx
+++ src/pages/case-page/components/media/VideoLegend/VideoLegend.tsx
@@ -1,7 +1,4 @@
 import styles from './videolegend.module.scss';
-import { useState } from 'react';
-import Skeleton from 'react-loading-skeleton';
-import 'react-loading-skeleton/dist/skeleton.css';
 
 type VideoLegendProps = {
     src: string;
@@ -9,27 +6,9 @@ type VideoLegendProps = {
 }
 
 function VideoLegend({ legend, src }: VideoLegendProps) {
-    const [videoLoaded, setVideoLoaded] = useState(false);
-
     return (
         <div className={styles.videoContainer}>
-            {!videoLoaded && (
-                <Skeleton
-                    height="300px"
-                    width="100%"
-                    className={styles.videoSkeleton}
-                    baseColor="#272727"
-                    highlightColor="#1d1d1d"
-                />
-            )}
-            <video 
-                src={src} 
-                muted 
-                loop 
-                autoPlay
-                className={videoLoaded ? styles.videoLoaded : styles.videoHidden}
-                onLoadedData={() => setVideoLoaded(true)}
-            />
+            <video src={src} muted loop autoPlay/>
             <p>{legend}</p>
         </div>
     )

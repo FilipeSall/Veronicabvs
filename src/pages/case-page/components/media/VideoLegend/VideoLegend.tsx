d23b7b1051b5b8bb53709379f99ae88fbecc08e2
Filipe Salles <filipe.salles@seatecnologia.com.br>
2025-09-10 10:25:19 -0300

    video legend skelletton

--- src/pages/case-page/components/media/VideoLegend/VideoLegend.tsx
+++ src/pages/case-page/components/media/VideoLegend/VideoLegend.tsx
@@ -1,4 +1,7 @@
 import styles from './videolegend.module.scss';
+import { useState } from 'react';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 
 type VideoLegendProps = {
     src: string;
@@ -6,9 +9,27 @@ type VideoLegendProps = {
 }
 
 function VideoLegend({ legend, src }: VideoLegendProps) {
+    const [videoLoaded, setVideoLoaded] = useState(false);
+
     return (
         <div className={styles.videoContainer}>
-            <video src={src} muted loop  autoPlay/>
+            {!videoLoaded && (
+                <Skeleton
+                    height="300px"
+                    width="100%"
+                    className={styles.videoSkeleton}
+                    baseColor="#272727"
+                    highlightColor="#1d1d1d"
+                />
+            )}
+            <video 
+                src={src} 
+                muted 
+                loop 
+                autoPlay
+                className={videoLoaded ? styles.videoLoaded : styles.videoHidden}
+                onLoadedData={() => setVideoLoaded(true)}
+            />
             <p>{legend}</p>
         </div>
     )
